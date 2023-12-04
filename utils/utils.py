02e79989d6c74eec574ef4e33575aeae9b118601
r-aashish <61154281+r-aashish@users.noreply.github.com>
2023-12-03 21:45:19 -0500

    Add files via upload

---
+++ utils/utils.py
@@ -0,0 +1,144 @@
+
+import torch.nn.functional as F
+import torch
+import random
+import numpy as np
+from multiprocessing import Process, Queue
+
+activation_getter = {'iden': lambda x: x, 'relu': F.relu, 'tanh': torch.tanh, 'sigm': torch.sigmoid}
+
+
+def gpu(tensor, gpu=False):
+
+    if gpu:
+        return tensor.cuda()
+    else:
+        return tensor
+
+
+def cpu(tensor):
+
+    if tensor.is_cuda:
+        return tensor.cpu()
+    else:
+        return tensor
+
+
+def minibatch(*tensors, **kwargs):
+
+    batch_size = kwargs.get('batch_size', 128)
+
+    if len(tensors) == 1:
+        tensor = tensors[0]
+        for i in range(0, len(tensor), batch_size):
+            yield tensor[i:i + batch_size]
+    else:
+        for i in range(0, len(tensors[0]), batch_size):
+            yield tuple(x[i:i + batch_size] for x in tensors)
+
+
+def shuffle(*arrays, **kwargs):
+
+    require_indices = kwargs.get('indices', False)
+
+    if len(set(len(x) for x in arrays)) != 1:
+        raise ValueError('All inputs to shuffle must have '
+                         'the same length.')
+
+    shuffle_indices = np.arange(len(arrays[0]))
+    np.random.shuffle(shuffle_indices)
+
+    if len(arrays) == 1:
+        result = arrays[0][shuffle_indices]
+    else:
+        result = tuple(x[shuffle_indices] for x in arrays)
+
+    if require_indices:
+        return result, shuffle_indices
+    else:
+        return result
+
+
+def assert_no_grad(variable):
+
+    if variable.requires_grad:
+        raise ValueError(
+            "nn criterions don't compute the gradient w.r.t. targets - please "
+            "mark these variables as volatile or not requiring gradients"
+        )
+
+
+def set_seed(seed, cuda=False):
+
+    np.random.seed(seed)
+    random.seed(seed)
+    if cuda:
+        torch.cuda.manual_seed(seed)
+    else:
+        torch.manual_seed(seed)
+
+
+def str2bool(v):
+    return v.lower() in ('true')
+
+def random_neq(l, r, s):
+    t = np.random.randint(l, r)
+    while t in s:
+        t = np.random.randint(l, r)
+    return t
+
+def sample_function(user_train, usernum, itemnum, batch_size, maxlen, result_queue, SEED):
+
+    def sample():
+        user = np.random.randint(1, usernum + 1)
+        while len(user_train[user]) <= 1: user = np.random.randint(1, usernum + 1)
+
+        seq = np.zeros([maxlen], dtype=np.int32)
+        pos = np.zeros([maxlen], dtype=np.int32)
+        neg = np.zeros([maxlen], dtype=np.int32)
+        nxt = user_train[user][-1]
+        idx = maxlen - 1
+
+        ts = set(user_train[user])
+        for i in reversed(user_train[user][:-1]):
+            seq[idx] = i
+            pos[idx] = nxt
+            if nxt != 0: neg[idx] = random_neq(1, itemnum + 1, ts)
+            nxt = i
+            idx -= 1
+            if idx == -1: break
+
+        return (user, seq, pos, neg)
+
+    np.random.seed(SEED)
+    while True:
+        one_batch = []
+        for i in range(batch_size):
+            one_batch.append(sample())
+        result_queue.put(zip(*one_batch))
+
+class WarpSampler(object):
+    def __init__(self, User, usernum, itemnum, batch_size=64, maxlen=10, n_workers=1):
+        self.result_queue = Queue(maxsize=n_workers * 10)
+        self.processors = []
+        for i in range(n_workers):
+            self.processors.append(
+                Process(target=sample_function, args=(User,
+                                                      usernum,
+                                                      itemnum,
+                                                      batch_size,
+                                                      maxlen,
+                                                      self.result_queue,
+                                                      np.random.randint(2e9)
+                                                      )))
+            self.processors[-1].daemon = True
+            self.processors[-1].start()
+
+    def next_batch(self):
+        return self.result_queue.get()
+
+    def close(self):
+        for p in self.processors:
+            p.terminate()
+            p.join()
+
